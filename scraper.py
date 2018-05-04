53a5feb5cc3e5798807746dc3a3c97b2561846b9
Jason Kao <jasonkao85@gmail.com>
2018-05-04 10:43:54 -0400

    participant names

--- scraper.py
+++ scraper.py
@@ -1,13 +1,20 @@
 from bs4 import BeautifulSoup as bs4
+
 import requests
 import re
 
 r = requests.get('http://www.presidency.ucsb.edu/ws/index.php?pid=119039')
 
-soup = bs4.soup(r.content, 'html.parser')
+soup = bs4(r.content, 'lxml')
 
 text = soup.find('span', attrs={'class': 'displaytext'})
 
+# Matches Schumer (D-NY)
 name_pattern = re.compile(r'(\S+)\s\((D|R)(\S+)?\)')
 
-for 
+participants = [
+    (name_pattern.search(i).group(1).upper(), name_pattern.search(i).group(2))
+    for i in text.find_all(string=name_pattern)
+]
+
+print participants
