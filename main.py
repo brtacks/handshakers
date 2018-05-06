3795c99abc3de94bb78be48f2054848cad98b985
Jason Kao <jasonkao85@gmail.com>
2018-05-06 14:02:11 -0400

    hardcoded debaters

--- main.py
+++ main.py
@@ -14,13 +14,126 @@ MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
           'August', 'September', 'October', 'November', 'December']
 
 PRESIDENTIAL = 'Presidential'
+VICE_PRESIDENTIAL = 'Vice Presidential'
 DEMOCRATIC = 'Democratic'
 REPUBLICAN = 'Republican'
 
 TITLE_PATTERNS = {
     PRESIDENTIAL: re.compile(r'(\S+)\s\((D|R)(\S+)?\)'),
+    VICE_PRESIDENTIAL: re.compile(r'(\S+)\s\((D|R)(\S+)?\)'),
     DEMOCRATIC: re.compile(r'(\w+)((\s\([\w\s]+\))|;)'),
     REPUBLICAN: re.compile(r'(\w+)((\s\([\w\s]+\))|;)'),
+}
+
+DEBATORS_BY_YEAR = {
+    2012: {
+        PRESIDENTIAL: {
+            'OBAMA': 'D',
+            'ROMNEY': 'R',
+        },
+        VICE_PRESIDENTIAL: {
+            'BIDEN': 'D',
+            'RYAN': 'R',
+        },
+    },
+    2008: {
+        PRESIDENTIAL: {
+            'OBAMA': 'D',
+            'MCCAIN': 'R',
+        },
+        VICE_PRESIDENTIAL: {
+            'BIDEN': 'D',
+            'PALIN': 'R',
+        },
+    },
+    2004: {
+        PRESIDENTIAL: {
+            'KERRY': 'D',
+            'BUSH': 'R',
+        }
+        VICE_PRESIDENTIAL: {
+            'EDWARDS': 'D',
+            'CHENEY': 'R',
+        },
+        DEMOCRATIC: ['CLARK', 'LIEBERMAN', 'KUCINICH', 'DEAN', 'KERRY',
+                     'SHARPTON', 'EDWARDS'],
+    },
+    2000: {
+        PRESIDENTIAL: {
+            'GORE': 'D',
+            'BUSH': 'R',
+        },
+        VICE_PRESIDENTIAL: {
+            'LIEBERMAN': 'D',
+            'CHENEY': 'R',
+        },
+    },
+    1996: {
+        PRESIDENTIAL: {
+            'CLINTON': 'D',
+            'DOLE': 'R',
+        },
+        VICE_PRESIDENTIAL: {
+            'GORE': 'D',
+            'KEMP': 'R',
+        },
+    },
+    1992: {
+        PRESIDENTIAL: {
+            'CLINTON': 'D',
+            'BUSH': 'R',
+        },
+        VICE_PRESIDENTIAL: {
+            'GORE': 'D',
+            'QUAYLE': 'R',
+        },
+    },
+    1988: {
+        PRESIDENTIAL: {
+            'DUKAKIS': 'D',
+            'BUSH': 'R',
+        },
+        VICE_PRESIDENTIAL: {
+            'BENTSEN': 'D',
+            'QUAYLE': 'R',
+        },
+    },
+    1984: {
+        PRESIDENTIAL: {
+            'MONDALE': 'D',
+            'REAGAN': 'R',
+        },
+        VICE_PRESIDENTIAL: {
+            'FERRARO': 'D',
+            'BUSH': 'R',
+        },
+    },
+    1980: {
+        PRESIDENTIAL: {
+            'CARTER': 'D',
+            'REAGAN': 'R',
+        },
+        VICE_PRESIDENTIAL: {
+            'LIEBERMAN': 'D',
+            'CHENEY': 'R',
+        },
+    },
+    1976: {
+        PRESIDENTIAL: {
+            'CARTER': 'D',
+            'FORD': 'R',
+        },
+        VICE_PRESIDENTIAL: {
+            'MONDALE': 'D',
+            'DOLE': 'R',
+        },
+    },
+    1960: {
+        PRESIDENTIAL: {
+            'KENNEDY': 'D',
+            'NIXON': 'R',
+        },
+    },
 }
 
 def collect_transcripts():
@@ -28,34 +141,26 @@ def collect_transcripts():
     soup = bs4(r.content, 'lxml')
     docdate = soup.find('span', attrs={'class': 'docdate'})
     table = docdate.parent.table.table
+    year = None
     for tr in table.find_all('tr'):
+        year_label = tr.find('td', attrs={'class':'roman'})
+        if year_label is not None:
+            year = int(year_label.text)
         if tr.a:
             url = tr.a.attrs['href']
-            create_transcript(url)
+            create_transcript(url, year)
 
 # create_transcript creates a transcript dictionary.
-def create_transcript(url):
+def create_transcript(url, year):
     soup = get_soup(url)
     transcript = init_transcript(soup, url)
-    transcript['debaters'] = get_debaters(soup, transcript['debate_type'])
+    transcript['debaters'] = get_debaters(soup, transcript['debate_type'], year)
     print_transcript(transcript)
 
 # get_debaters finds each candidate and every line they spoke.
-def get_debaters(soup, debate_type):
+def get_debaters(soup, debate_type, year):
     text = soup.find('span', attrs={'class': 'displaytext'})
-    title_pattern = TITLE_PATTERNS[debate_type]
-    debaters = {}
-    for i in text.children:
-        if i.name == 'p':
-            break
-        line = str(i)
-        match = title_pattern.search(line)
-        if match:
-            debaters[match.group(1).upper()] = {
-                'lines': [0],
-                'party': match.group(2) if debate_type == PRESIDENTIAL
-                else debate_type[0]
-            }
+    debaters = find_debaters(text, debate_type, year)
     if len(debaters) == 0:
         return debaters
     lines = text.find_all('p')
