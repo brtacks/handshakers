301186326ab379e21dc2a0af03c444f9ce4dee91
Jason Kao <jasonkao85@gmail.com>
2018-05-15 20:42:49 -0400

    tired

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import re
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 MF_DICT_FNAME = 'data/raw/mf_dict.txt'
 
@@ -42,8 +45,10 @@ def get_pattern(word):
     )
 
 
-def analyze_corpus(corpus):
-    lines = filter(None, corpus.split('\n')[1:])
+# construct_corpus constructs a corpus consisting of words from the Democratic
+# and Republican candidates in a body of text.
+def construct_corpus(text):
+    lines = filter(None, text.split('\n')[1:])
     divider = lines.index('%')
     header, body = lines[:divider], lines[divider+1:]
     dems, reps = [], []
@@ -64,10 +69,14 @@ def analyze_corpus(corpus):
             dem_corpus += line
         elif is_rep:
             rep_corpus += line
+    dem_corpus = dem_corpus.decode('utf-8')
+    rep_corpus = rep_corpus.decode('utf-8')
 
-    return find_instances(dem_corpus.decode('utf-8')), find_instances(rep_corpus.decode('utf-8'))
+    return find_instances(dem_corpus), find_instances(rep_corpus)
 
 
+# to_excel converts a corpus of Democratic and Republican words into an Excel
+# spreadsheet formatted for perspective analysis.
 def to_excel(dem_words, rep_words):
     writer = pd.ExcelWriter('data/excel/output.xlsx', engine='xlsxwriter')
     pd.DataFrame(
@@ -81,11 +90,10 @@ def to_excel(dem_words, rep_words):
 
     workbook = writer.book
     text_format = workbook.add_format({'text_wrap': True})
-    align = workbook.add_format({'valign': 'bottom'})
     for sheet in writer.sheets.values():
-        sheet.set_column('A:A', None, align)
         sheet.set_column('B:B', 12.5, text_format)
         sheet.set_column('C:C', 67, text_format)
+
     writer.save()
 
 
@@ -102,12 +110,34 @@ def find_instances(corpus):
                     'word': w['word'],
                     'instances': instances,
                     'foundations': w['foundations'],
+                    'frequency': len(instances) * 1.0 / len(corpus)
                 })
     return words
 
 
-# spread_words creates a pandas-compatible data structure with each instance of
-# each word as its own row.
+# find_sig_diffs finds the words in a corpus's two word lists that significantly
+# differ in frequency.
+def find_sig_diffs(dem_words, rep_words):
+    dem_stems = {x['word']: x for x in dem_words}
+    rep_stems = {x['word']: x for x in rep_words}
+    diffs = []
+    for w in WORDS:
+        stem = w['word']
+        if stem in dem_stems and stem in rep_stems:
+            diffs.append(dem_stems[stem]['frequency'] - rep_stems[stem]['frequency'])
+        elif stem in dem_stems:
+            diffs.append(dem_stems[stem]['frequency'])
+        elif stem in rep_stems:
+            diffs.append(rep_stems[stem]['frequency'])
+    xbar = np.mean(diffs)
+    s = np.std(diffs)
+    z_scores = [(x-xbar)*1.0 / s for x in diffs]
+    _, _, _ = plt.hist(z_scores)
+    plt.show()
+
+
+# spread_words reduces a word list into  a pandas-compatible data structure
+# with one instance of each word as its own row.
 def spread_words(words):
     data = []
     for w in words:
@@ -124,8 +154,16 @@ def spread_words(words):
 
 if __name__ == '__main__':
     init_mf_dict()
+    """
     fname = 'data/raw/Presidential-2012-10-16.txt'
     with open(fname) as f:
         s = f.read()
-        dem_words, rep_words = analyze_corpus(s)
-        to_excel(dem_words, rep_words)
+        dem_words, rep_words = construct_corpus(s)
+        w = open('cac.he', 'w')
+        w.write(str(dem_words))
+        w.write('%')
+        w.write(str(rep_words))
+    """
+    with open('cac.he') as f:
+        [dem_words, rep_words] = [eval(x) for x in f.read().split('%')]
+        find_sig_diffs(dem_words, rep_words)
